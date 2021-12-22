0f6c9f056a10e43f8eeaf7a5abbca42f345c7d86
besmelh <besmelh.s@gmail.com>
2021-12-22 18:53:18 +0300

    video player added for app dev page

---
+++ my-website/src/Components/AppDevPageSection.js
@@ -0,0 +1,55 @@
+import {React, useState} from "react";
+import LeftRightContainer from "./LeftRightContainer";
+import VideoPlayer from "./VideoPlayer";
+import IconAndTitle from "./IconAndTitle";
+import MyButton from "./MyButton";
+import { Icon } from "@iconify/react";
+
+
+function AppDevPageSection(props) {
+
+    const {largeContainerLeft, image, videoUrl, icon, title, description, description2, tinytext, buttonIcon, buttonText, ...other } = props;
+    const [showVideo, setShowVideo] = useState(false);
+
+    return (
+        <LeftRightContainer {...other}>
+
+        {!largeContainerLeft && 
+            <div className="container-medium">
+                <img src={require(`../Images/${image}`).default} alt= {`screenshot of ${title}`}/>
+                <VideoPlayer url = {videoUrl} style={showVideo ? {display:"flex"} : {display:"none"}}/> 
+            </div>
+        }
+
+
+        <div className="container-small">
+           <IconAndTitle width="50px">
+            <img src={require(`../Images/${icon}`).default} alt= {`icon of ${title}`}/>
+            <h2>{title}</h2>
+          </IconAndTitle>
+
+          <p>{description}</p>    
+          <p>{description2}</p>     
+          <p className="tinytext">{tinytext}</p>
+
+          <MyButton onClick={()=>setShowVideo(!showVideo)}>
+              {console.log("showVideo: ", showVideo)}
+            <Icon icon={buttonIcon || "bi:play-circle"} inline={true} />
+            {buttonText || "In Action"}
+          </MyButton>
+          
+        </div> 
+
+        {largeContainerLeft && 
+            <div className="container-medium">
+                <img src={require(`../Images/${image}`).default} alt= {`screenshot of ${title}`}/>
+                <VideoPlayer url = {videoUrl} style={showVideo ? {display:"flex"} : {display:"none"}}/> 
+            </div>
+        }
+        </LeftRightContainer>
+    );
+  }
+  
+  export default AppDevPageSection;
+      
+      
--- my-website/src/Pages/AppDevelopment.js
+++ my-website/src/Pages/AppDevelopment.js
@@ -1,16 +1,12 @@
-import styled from 'styled-components';
+import React from 'react';
 import Header from '../Components/Header';
 import Content from '../Components/Content';
-import LeftRightContainer from '../Components/LeftRightContainer';
-import MyButton from '../Components/MyButton';
-import IconAndTitle from '../Components/IconAndTitle';
-import VideoPlayer from '../Components/VideoPlayer';
-import { Icon } from '@iconify/react';
+import AppDevPageSection from '../Components/AppDevPageSection';
 
 
 function AppDevelopment() {
 
-  
+const lightupDesc = "LightUp allows users to brainstorm new ideas by generating random words to inspire them. If they like a specific word it can be locked to prevent it from changing when generating new words. Users can also make notes of all their ideas they get so you don't forget them. {'\n'} Currently available in the App Store, and is localized in English & Arabic."
   return (
     <div className="AppDevelopment">
       <Header>
@@ -21,95 +17,50 @@ function AppDevelopment() {
       <Content>
 
         {/* ********* light up ********* */}
-        <LeftRightContainer>
-        <div className="container-small">
-          <IconAndTitle width="50px">
-            <img src={require(`../Images/lightup-icon.png`).default}/>
-            <h2>Light Up</h2>
-          </IconAndTitle>
-
-          <p>LightUp allows users to brainstorm new ideas by generating random words to inspire them. If they like a specific word it can be locked to prevent it from changing when generating new words. Users can also make notes of all their ideas they get so you don't forget them. <br/><br/> Currently available in the App Store, and is localized in English & Arabic.</p>
-          
-          <MyButton  onClick={()=> window.open("https://www.google.com/", "_blank")}>
-            <Icon icon="ant-design:apple-filled" inline={true} />
-            View on App Store
-          </MyButton>
-        </div>
-
-        <div className="container-medium">
-          <img src={require(`../Images/lightup-ss.png`).default}/>
-        </div>
-        </LeftRightContainer>
+        <AppDevPageSection
+          image = "lightup-ss.png"
+          videoUrl="https://www.youtube.com/watch?v=nWvXHZj4lm0"
+          icon = "lightup-icon.png"
+          title="Light Up"
+          description={`LightUp allows users to brainstorm new ideas by generating random words to inspire them. If they like a specific word it can be locked to prevent it from changing when generating new words. Users can also make notes of all their ideas they get so you don't forget them.`}
+          description2="Currently available in the App Store, and is localized in English & Arabic."          
+          buttonText= "View on the App Store"
+          buttonIcon="ant-design:apple-filled"
+          largeContainerLeft
+        />
 
 
         {/* ********* starship adventure ********* */}
-        <LeftRightContainer>
-        <div className="container-medium">
-          <img src={require(`../Images/starshipadventure-ss.png`).default}/>
-          <VideoPlayer url ="https://www.youtube.com/watch?v=nWvXHZj4lm0" style ={{display:"none"}}/>
-        </div>
-
-        <div className="container-small">
-          <IconAndTitle width="50px">
-            <img src={require(`../Images/starshipadventure-icon.png`).default}/>
-            <h2>Starship Adventure</h2>
-          </IconAndTitle>
-
-          <p>Starship Adventure is a game where the user tries to find the correct angle and velocity to fly the rocket from Earth and land safely on Mars. On each round Mars is placed at a different location.</p>          
-          <p className="tinytext">This app was made by my team and I for our 48-hour Hackathon submission for CSC214 - Fall 2020 - University of Rochester</p>
-
-          <MyButton>
-          <Icon icon="bi:play-circle" inline={true} />
-            In Action
-          </MyButton>
-        </div>
-        </LeftRightContainer>
+        <AppDevPageSection
+          image = "starshipadventure-ss.png"
+          videoUrl="https://www.youtube.com/watch?v=nWvXHZj4lm0"
+          icon = "starshipadventure-icon.png"
+          title="Starship Adventure"
+          description="Starship Adventure is a game where the user tries to find the correct angle and velocity to fly the rocket from Earth and land safely on Mars. On each round Mars is placed at a different location." 
+          tinytext="This app was made by my team and I for our 48-hour Hackathon submission for CSC214 - Fall 2020 - University of Rochester"
+        />
 
         {/* ********* watch what ********* */}
-        <LeftRightContainer>
-        <div className="container-small">
-          <IconAndTitle width="50px">
-            <img src={require(`../Images/watchwhat-icon.png`).default}/>
-            <h2>Watch What?</h2>
-          </IconAndTitle>
-
-          <p>Can't decide on what to watch next? Open the "Watch What" app and swipe through the randomly generated T.V. shows, and double tap on the ones you like to save them to your favorites list!</p>
-          <p className="tinytext">This app was made as a project for CSC214 - Fall 2020 - University of Rochester</p>
-
-          <MyButton>
-          <Icon icon="bi:play-circle" inline={true} />
-            In Action
-          </MyButton>
-        </div>
-
-        <div className="container-medium">
-          <img src={require(`../Images/watchwhat-ss.png`).default}/>
-        </div>
-        </LeftRightContainer>
-
+        <AppDevPageSection
+          image = "watchwhat-ss.png"
+          videoUrl="https://www.youtube.com/watch?v=nWvXHZj4lm0"
+          icon = "watchwhat-icon.png"
+          title="Watch What?"
+          description={`Can't decide on what to watch next? Open the "Watch What" app and swipe through the randomly generated T.V. shows, and double tap on the ones you like to save them to your favorites list!`}
+          tinytext="This app was made as a project for CSC214 - Fall 2020 - University of Rochester"
+          largeContainerLeft
+        />
 
         {/* ********* trip rate ********* */}
-        <LeftRightContainer>
-        <div className="container-medium">
-          <img src={require(`../Images/triprate-ss.png`).default}/>
-        </div>
 
-        <div className="container-small">
-          <IconAndTitle width="50px">
-            <img src={require(`../Images/triprate-icon.png`).default}/>
-            <h2>TripRate</h2>
-          </IconAndTitle>
-
-          <p>The app allows the user to log their trips' information, including the trip's name, description, type, number of people accompanied, and the amount of fun they had. There's also a status tab to track information regarding all the trips.</p>          
-          <p className="tinytext">This app was made as a project for CSC214 - Fall 2020 - University of Rochester</p>
-
-          <MyButton>
-          <Icon icon="bi:play-circle" inline={true} />
-            In Action
-          </MyButton>
-        </div>
-        </LeftRightContainer>
-
+        <AppDevPageSection
+          image = "triprate-ss.png"
+          videoUrl="https://www.youtube.com/watch?v=nWvXHZj4lm0"
+          icon = "triprate-icon.png"
+          title="TripRate"
+          description={`The app allows the user to log their trips' information, including the trip's name, description, type, number of people accompanied, and the amount of fun they had. There's also a status tab to track information regarding all the trips.`}
+          tinytext="This app was made as a project for CSC214 - Fall 2020 - University of Rochester"
+        />
 
       </Content>
 

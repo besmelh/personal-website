8d3377b231c9235b0af67cdd999b84cf539086ae
besmelh <besmelh.s@gmail.com>
2022-01-13 03:49:27 +0300

    web dev icon added

--- src/Pages/Demo.js
+++ src/Pages/Demo.js
@@ -10,10 +10,16 @@ function Demo() {
     <div class="background-container" style={{position:"relative", width: "100%", height: "100%"}}>
         <StaryBackground/>
         <AstronautModel />
-        <AstronautModel />
-        <AstronautModel />
-        <AstronautModel />
-        <AstronautModel />
+        <model-viewer
+          src="models/web-dev-planet.glb"
+          ios-src=""
+          poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
+          alt="A 3D model of an astronaut"
+          shadow-intensity="1"
+          auto-rotate
+          ar
+          style={{height: "500px", width: "500px"}}
+        ></model-viewer>
     </div>
   );
 }

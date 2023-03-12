616f44323f633892258552b3594de3fb6bbfa23c
Besmelh Alshaalan <besmelh.s@gmail.com>
2023-03-11 23:26:12 -0500

    loading on model

--- src/Components/PlanetDisplay.js
+++ src/Components/PlanetDisplay.js
@@ -5,30 +5,37 @@ import FadeIn from 'react-fade-in';
 // import Spline from '@splinetool/react-spline';
 const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
+const scale = '1';
+const width = '400';
+const height = '400';
+const width_clickable = width / 1.2;
+const height_clickable = height / 1.5;
+
 const OuterDiv = styled.div`
   position: relative;
-  background-color: red;
-  width: 100%;
-  height: 400px;
+  /* background-color: red; */
+  width: ${width}px;
+  height: ${height}px;
 
   canvas {
-    scale: 0.8;
-    height: 400px;
+    scale: ${scale};
+    height: ${height}px;
+    /* background-color: green; */
   }
 
   .splineDiv {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 400px;
+    height: ${height}px;
   }
 `;
 
 const InnerDiv = styled.div`
   position: absolute;
-  width: 100%;
-  height: 400px;
-  background-color: pink;
+  width: ${width}px;
+  height: ${height}px;
+  /* background-color: pink; */
   display: flex;
   align-items: center;
   justify-content: center;
@@ -37,15 +44,22 @@ const InnerDiv = styled.div`
 const LoadingOuterDiv = styled.div`
   position: relative;
   text-align: center;
-  background-color: green;
-  height: 400px;
+  background-color: grey;
+  height: ${height}px;
 
-  p {
+  p,
+  img {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
   }
+`;
+
+const ClickableZone = styled.div`
+  width: ${width_clickable}px;
+  height: ${height_clickable}px;
+  /* background-color: darkGrey; */
 `;
 
 function PlanetDisplay(props) {
@@ -64,21 +78,29 @@ function PlanetDisplay(props) {
         </FadeIn>
       </InnerDiv>
 
-      {/* <InnerDiv>
+      {/* {isModelLoading && (
+        <InnerDiv>
+          <FadeIn>
+            <LoadingOuterDiv>
+              <img src={image} />
+              <p>Loading...</p>
+            </LoadingOuterDiv>
+          </FadeIn>
+        </InnerDiv>
+      )} */}
+
+      <InnerDiv>
         <FadeIn>
           <LoadingOuterDiv>
             <img src={image} />
             <p>Loading...</p>
           </LoadingOuterDiv>
         </FadeIn>
-      </InnerDiv> */}
+      </InnerDiv>
 
-      {/* {isModelLoading && (
-        <FadeIn>
-          <img src={image} />
-          <p>Loading...</p>
-        </FadeIn>
-      )} */}
+      {/* <InnerDiv>
+        <ClickableZone />
+      </InnerDiv> */}
     </OuterDiv>
   );
 }

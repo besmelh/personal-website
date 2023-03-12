28cd3c9f307d1f37c2750946e8ef607acd2a687e
Besmelh Alshaalan <besmelh.s@gmail.com>
2023-03-11 20:54:22 -0500

    darker smaller stars

--- src/Components/RandomStar.js
+++ src/Components/RandomStar.js
@@ -1,48 +1,45 @@
 import React, { useRef } from 'react';
 import styled from 'styled-components';
 
-
-
 const Star = styled.div`
-    border-radius: 100%;
-    background-color: #F7F5FF;
-    position:absolute;
-    z-index: -5;
-`    
-    
+  border-radius: 100%;
+  background-color: #aaa6bd;
+  position: absolute;
+  z-index: -5;
+`;
+
 const RandomStar = (props) => {
+  const minSize = 1;
+  const maxSize = 5;
+  const minDur = 3;
+  const maxDur = 8;
+  const minDel = 0.1;
+  const maxDel = 2;
+  const maxHeight = 90;
+  const maxHeightPhone = 30;
 
-    const minSize = 2;
-    const maxSize = 10;
-    const minDur = 3;
-    const maxDur = 8;
-    const minDel = 0.1;
-    const maxDel = 2;
-    const maxHeight = 90;
-    const maxHeightPhone = 30;
+  const randHeight = Math.floor(Math.random() * maxHeight);
+  //     const randHeightPhone = Math.floor(Math.random()*maxHeightPhone);
+  const randWidth = Math.floor(Math.random() * 100);
+  const randSize = Math.floor(
+    Math.random() * (maxSize - minSize + 1) + minSize
+  );
 
-    const randHeight = Math.floor(Math.random()*maxHeight);
-//     const randHeightPhone = Math.floor(Math.random()*maxHeightPhone);
-    const randWidth = Math.floor(Math.random()*100);
-    const randSize = Math.floor(Math.random()* (maxSize - minSize + 1) + minSize);
+  const randAnimDuration = Math.random() * (maxDur - minDur + 1) + minDur;
+  const randAnimDelay = Math.random() * (maxDel - minDel + 1) + minDel;
 
-    const randAnimDuration = Math.random() * (maxDur - minDur + 1) + minDur;
-    const randAnimDelay= Math.random() * (maxDel - minDel + 1) + minDel;
+  const StarStyle = useRef({
+    height: `${randSize}px`,
+    width: `${randSize}px`,
+    top: `${randHeight}%`,
+    left: `${randWidth}%`,
+    transform: 'translate(-50%, -50%)',
+    zindex: '-10',
+    animation: `flickerAnimation ${randAnimDuration}s infinite`,
+    animationDelay: `${randAnimDelay}s`,
+  });
 
-   const StarStyle = useRef({
-        height: `${randSize}px`,
-        width: `${randSize}px`,
-        top: `${randHeight}%`,
-        left: `${randWidth}%`,
-        transform: 'translate(-50%, -50%)',
-        zindex: '-10',
-        animation: `flickerAnimation ${randAnimDuration}s infinite`, 
-        animationDelay: `${randAnimDelay}s`,
-   });
+  return <Star style={StarStyle.current} {...props} />;
+};
 
-   return (
-        <Star style={StarStyle.current} {...props} />
-   )
-}
-    
 export default RandomStar;

e93396ee5d4366a3f777e744135d67a0aaa328f6
Besmelh Alshaalan <besmelh.s@gmail.com>
2023-03-13 23:08:50 -0400

    lightup page

--- src/Pages/LightUp.js
+++ src/Pages/LightUp.js
@@ -3,6 +3,19 @@ import Header from '../Components/Header';
 import Content from '../Components/Content';
 import ScrollToTop from '../Components/ScrollToTop';
 import PlanetDisplay_LightUp from '../Components/PlanetDisplay_LightUp';
+import styled from 'styled-components';
+import Divider from '../Components/Divider';
+import LeftRightContainer from '../Components/LeftRightContainer';
+import VideoPlayer from '../Components/VideoPlayer';
+
+const ImageRow = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  & img {
+    width: 20%;
+  }
+`;
 
 function LightUp(props) {
   const transition = { duration: 0.5, type: 'spring' };
@@ -18,25 +31,41 @@ function LightUp(props) {
           <div>
             <h2>LightUp</h2>
             <h4>
-              Below are some of the iOS apps I made with Swift, and VR projects
-              made with Unity.
+              An iOS app for brainstorming ideas; it displays random words and
+              has the option to write ideas in "sticky notes".
             </h4>
           </div>
         </Header>
-
+        <Divider />
         <div>
-          <Content>
+          <Content style={{ marginTop: '50px' }}>
             {/* ********* lightup ********* */}
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem. Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
+            {/* <LeftRightContainer>
+              <div className='container-small'>
+                <p>
+                  in my efforts to apply my mobile app development knowledge
+                  outside of class, LightUp was born after I had no idea of what
+                  app ideas to develop. Its main goal is to help the user
+                  brainstorm ideas and keep a note of them.
+                </p>
+              </div>
+              <div className='container-medium' style={{ height: '500px' }}>
+                <VideoPlayer url='https://youtu.be/P9AHAqrAPxU' />
+              </div>
+            </LeftRightContainer>
+            <Divider /> */}
+            <p style={{ marginBottom: '50px' }}>
+              in my efforts to apply my mobile app development knowledge outside
+              of class, LightUp was born after I had no idea of what app ideas
+              to develop. Its main goal is to help the user brainstorm ideas and
+              keep a note of them.
             </p>
+            <ImageRow>
+              <img src='https://cdn.glitch.global/8ab04539-a012-487a-82a4-7612329b440c/lightup-sc-1.webp?v=1678761996650' />
+              <img src='https://cdn.glitch.global/8ab04539-a012-487a-82a4-7612329b440c/lightup-sc-2.webp?v=1678761996304' />
+              <img src='https://cdn.glitch.global/8ab04539-a012-487a-82a4-7612329b440c/lightup-sc-3.webp?v=1678761995965' />
+              <img src='https://cdn.glitch.global/8ab04539-a012-487a-82a4-7612329b440c/lightup-sc-4.webp?v=1678761995427' />
+            </ImageRow>
           </Content>
         </div>
       </ScrollToTop>

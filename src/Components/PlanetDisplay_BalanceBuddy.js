c3b8f7bd81a5744fba121d5cde9bf6c6bc58d7d9
Besmelh Alshaalan <besmelh.s@gmail.com>
2023-12-26 10:51:23 +0300

    fixed balancebuddy 3d model

--- src/Components/PlanetDisplay_BalanceBuddy.js
+++ src/Components/PlanetDisplay_BalanceBuddy.js
@@ -6,7 +6,7 @@ function PlanetDisplay_BalanceBuddy(props) {
   const { hoverable } = props;
   return (
     <PlanetDisplay
-      scene='https://prod.spline.design/Q7TUOjInZ3KxPOXL/scene.splinecode'
+      scene='https://prod.spline.design/uKjBs28q2kCXzTmW/scene.splinecode'
       placeholder='https://cdn.glitch.global/8ab04539-a012-487a-82a4-7612329b440c/balancebuddy-placeholder.png?v=1678730286600'
       href='balancebuddy'
       hoverable={hoverable}
